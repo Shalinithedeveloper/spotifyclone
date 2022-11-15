99e1eb3445fa8c3a431d64fd182e734adbaa8982
SHALINI <drshaverma242@gmail.com>
2022-11-15 22:27:44 +0530

    project

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Welcome to spotify");
+
+//Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+
+let songs = [
+    { songName: "Salam-e-Ishq", filePath: "new/1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "new/2.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "new/3.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "new/1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "new/1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "new/1.mp3", coverPath: "covers/1.jpg" },
+]
+
+// audioElement.play();
+
+//Handle play/pause click
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.remove('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', () => {
+    //update seekbar 
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100);
+    myProgressBar.value = progress;
+
+})
