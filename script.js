3dc72736520f7adc1a8b7f4fd54e92f654647c10
SHALINI <drshaverma242@gmail.com>
2022-11-18 21:54:43 +0530

    project

--- script.js
+++ script.js
@@ -2,26 +2,37 @@ console.log("Welcome to spotify");
 
 //Initialize the Variables
 let songIndex = 0;
-let audioElement = new Audio('1.mp3');
+let audioElement = new Audio('new/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    { songName: "Salam-e-Ishq", filePath: "new/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "new/2.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "new/3.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "new/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "new/1.mp3", coverPath: "covers/1.jpg" },
-    { songName: "Salam-e-Ishq", filePath: "new/1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Kesariya", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/6.mp3", coverPath: "covers/6.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/7.mp3", coverPath: "covers/7.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/8.mp3", coverPath: "covers/8.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/9.mp3", coverPath: "covers/9.jpg" },
+    { songName: "Salam-e-Ishq", filePath: "songs/10.mp3", coverPath: "covers/10.jpg" },
 ]
 
+songItems.forEach((element, i)=>{
+    console.log(element, i);
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innertext = songs[i].songName;
+
+})
 // audioElement.play();
 
 //Handle play/pause click
 masterPlay.addEventListener('click', () => {
     if (audioElement.paused || audioElement.currentTime <= 0) {
-        
+
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
         gif.style.opacity = 1;
@@ -36,7 +47,16 @@ masterPlay.addEventListener('click', () => {
 // Listen to Events
 audioElement.addEventListener('timeupdate', () => {
     //update seekbar 
-    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100);
+    progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
     myProgressBar.value = progress;
+
+})
+
+myProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
+})
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach(()=>{
+    element.addEventListener('click')    
 
 })
